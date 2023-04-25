b251787b3b472b8fd10deb9f6575de5bff16fdff
RemyXVX <remyfranciscoflores@gmail.com>
2023-04-25 00:08:26 -0700

    refactor homepage to render components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,20 @@
 import { Inter } from 'next/font/google'
-import Link from 'next/link';
+import React from 'react';
+
+import Navibar from './components/Navibar';
+import Info from './components/Info';
+import Header from './components/Header';
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+const Home = () => {
   return(
-    <main className="py-4 px-48">
-      <h1>Home</h1>
-      <Link className="bg-teal-700 text-black font-medium py-2 px-4 rounded-md" href={"./components/about"}>Go to about</Link>
-    </main>
-  )
+    <React.Fragment>
+    <Header />
+    <Info />
+    <Navibar />
+  </React.Fragment>
+  );
 }
+
+export default Home;
