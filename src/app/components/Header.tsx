e224ce3e781bada6087856f36735e70b91976e0e
RemyXVX <remyfranciscoflores@gmail.com>
2023-04-25 00:06:52 -0700

    Added a navigation bar for homepage with links

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = () => {
+  return(
+    <h1 className="py-4 px-48">Welcome to Cringe!</h1>
+  )
+}
+
+export default Header;
---
+++ src/app/components/Info.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Link from "next/link";
+
+const Info = () => {
+  return(
+    <main className="py-4 px-48">
+      <p>Welcome to our social media site, inspired by the golden age of platforms like Tumblr and Myspace. We believe in connection, creativity, and self-expression, and offer a welcoming community of like-minded individuals. Join us to customize your profile, connect with friends, and share your creations with the world.</p>
+    </main>
+  )
+}
+
+export default Info;
