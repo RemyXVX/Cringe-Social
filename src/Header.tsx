179ac59d98c14d45cc2b4aa66518c0d427545d27
RemyXVX <remyfranciscoflores@gmail.com>
2023-05-20 15:59:05 -0700

    Minor update on project this will allow for the changes on commits mentioned before

---
+++ src/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return(
+    <h1 
+      className="py-4 px-60 text-5xl hover:font-semibold text-center"
+      >Welcome to Cringe!</h1>
+  )
+}
+
+export default Header;
---
+++ src/Info.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Info = () => {
+  return (
+    <main className="py-4 px-6 md:px-10">
+      <p className="py-4 px-2 md:px-12 flex-auto justify-end">
+        Welcome to our social media site, inspired by the golden age of platforms like Tumblr and Myspace. We believe in connection, creativity, and self-expression, and offer a welcoming community of like-minded individuals. Join us to customize your profile, connect with friends, and share your creations with the world.
+      </p>
+    </main>
+  );
+};
+
+export default Info;
